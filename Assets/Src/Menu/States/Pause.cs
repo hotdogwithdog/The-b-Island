8d1dfac6ecb13b9cba85c4f2a5e7bc1f5327046a
hotdogwithdog <hotdogwithdog@gmail.com>
2024-06-04 16:01:07 +0200

    ops estaba en la rama equivocada haciendo menus hago commit con este poco y voy a la rama correcta

---
+++ Assets/Src/Menu/States/Pause.cs
@@ -0,0 +1,31 @@
+﻿using Menu.Enums;
+using UnityEngine;
+using UnityEditor;
+
+namespace Menu.States
+{
+    public class Pause : AMenuState
+    {
+        public Pause() : base("Menus/Pause") { }
+
+        protected override void OnMenuNavigation(MenuButtons type)
+        {
+            switch (type)
+            {
+                case MenuButtons.Options:
+                    MenuManager.Instance.SetState(new Options());
+                    break;
+                case MenuButtons.Back:
+                    //MenuManager.Instance.SetState(new Gameplay());
+                    break;
+                default:
+                    Debug.LogError("ERROR UNKONW MENUBUTTON TYPE: " + type);
+                    return;
+            }
+        }
+
+        public override void Update(float deltaTime)
+        {
+        }
+    }
+}
